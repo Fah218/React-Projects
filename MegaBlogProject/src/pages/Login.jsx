99c9f860aeedd6aaf477d404f7822e690439c3a2
Fahad Furquan <fahadsaniya666@gmail.com>
2026-02-03 01:53:31 +0530

    Fix redux action import and login component casing

--- MegaBlogProject/src/pages/Login.jsx
+++ MegaBlogProject/src/pages/Login.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
 import LoginComponent from '../components/Login'
+
 function Login() {
   return (
-    <div className='py-8'>
-        <LoginComponent />
+    <div className="py-8">
+      <LoginComponent />
     </div>
   )
 }
-
-export default Login
